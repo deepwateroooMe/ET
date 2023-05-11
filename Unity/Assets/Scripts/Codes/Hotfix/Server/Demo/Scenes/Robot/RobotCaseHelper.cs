8f64ba2b93208f8e93224556f1e66e5724382f5c
deepwateroooMe <deepwaterooome@gmail.com>
2023-05-11 11:20:39 -0700

    LOVE MY DEAR COUSIN. MUST MARRY HIM AS SOON AS POSSIBLE~~

--- Unity/Assets/Scripts/Codes/Hotfix/Server/Demo/Scenes/Robot/RobotCaseHelper.cs
+++ Unity/Assets/Scripts/Codes/Hotfix/Server/Demo/Scenes/Robot/RobotCaseHelper.cs
@@ -66,7 +66,7 @@ namespace ET.Server
             {
                 clientScene = await Client.SceneFactory.CreateClientScene(zone, name);
                 await Client.LoginHelper.Login(clientScene, zone.ToString(), zone.ToString());
-                await Client.EnterMapHelper.EnterMapAsync(clientScene);
+                await Client.EnterRoomHelper.EnterRoomAsync(clientScene);
                 Log.Debug($"create robot ok: {zone}");
                 return clientScene;
             }
