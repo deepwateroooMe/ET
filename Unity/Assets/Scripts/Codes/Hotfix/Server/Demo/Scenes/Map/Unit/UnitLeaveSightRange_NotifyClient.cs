5ec4aa31fc89f75bb00f5a5bb5b4d05353ad9505
deepwateroooMe <deepwaterooome@gmail.com>
2023-06-01 17:07:24 -0700

    LOVE MY DEAR COUSIN. MUST MARRY HIM AS SOON AS POSSIBLE~~

--- Unity/Assets/Scripts/Codes/Hotfix/Server/Demo/Scenes/Map/Unit/UnitLeaveSightRange_NotifyClient.cs
+++ Unity/Assets/Scripts/Codes/Hotfix/Server/Demo/Scenes/Map/Unit/UnitLeaveSightRange_NotifyClient.cs
@@ -1,19 +1,14 @@
-﻿namespace ET.Server
-{
-    // 离开视野
+﻿namespace ET.Server {
+    // 离开视野: 大概是说，场景里的某个小人走到视野里看不见的地方
     [Event(SceneType.Map)]
-    public class UnitLeaveSightRange_NotifyClient: AEvent<EventType.UnitLeaveSightRange>
-    {
-        protected override async ETTask Run(Scene scene, EventType.UnitLeaveSightRange args)
-        {
+    public class UnitLeaveSightRange_NotifyClient: AEvent<EventType.UnitLeaveSightRange> {
+        protected override async ETTask Run(Scene scene, EventType.UnitLeaveSightRange args) {
             await ETTask.CompletedTask;
             AOIEntity a = args.A;
             AOIEntity b = args.B;
-            if (a.Unit.Type != UnitType.Player)
-            {
+            if (a.Unit.Type != UnitType.Player) {
                 return;
             }
-
             MessageHelper.NoticeUnitRemove(a.GetParent<Unit>(), b.GetParent<Unit>());
         }
     }
--- Unity/Assets/Scripts/Codes/Hotfix/Server/Demo/Scenes/Robot/RobotConsoleHandler.cs
+++ Unity/Assets/Scripts/Codes/Hotfix/Server/Demo/Scenes/Robot/RobotConsoleHandler.cs
@@ -1,60 +1,48 @@
 using System;
 using System.Reflection;
-
-namespace ET.Server
-{
+namespace ET.Server {
     [ConsoleHandler(ConsoleMode.Robot)]
-    public class RobotConsoleHandler: IConsoleHandler
-    {
-        public async ETTask Run(ModeContex contex, string content)
-        {
+    public class RobotConsoleHandler: IConsoleHandler {
+        public async ETTask Run(ModeContex contex, string content) {
             string[] ss = content.Split(" ");
-            switch (ss[0])
-            {
-                case ConsoleMode.Robot:
-                    break;
-
-                case "Run":
-                {
-                    int caseType = int.Parse(ss[1]);
-
+            switch (ss[0]) {
+            case ConsoleMode.Robot:
+                break;
+            case "Run": {
+                int caseType = int.Parse(ss[1]);
+                try {
+                    RobotLog.Debug($"run case start: {caseType}");
+                    await EventSystem.Instance.Invoke<RobotInvokeArgs, ETTask>(caseType, new RobotInvokeArgs() { Content = content });
+                    RobotLog.Debug($"run case finish: {caseType}");
+                }
+                catch (Exception e) {
+                    RobotLog.Debug($"run case error: {caseType}\n{e}");
+                }
+                break;
+            }
+            case "RunAll": {
+                FieldInfo[] fieldInfos = typeof (RobotCaseType).GetFields();
+                foreach (FieldInfo fieldInfo in fieldInfos) {
+                    int caseType = (int)fieldInfo.GetValue(null);
+                    if (caseType > RobotCaseType.MaxCaseType)
+                    {
+                        RobotLog.Debug($"case > {RobotCaseType.MaxCaseType}: {caseType}");
+                        break;
+                    }
                     try
                     {
                         RobotLog.Debug($"run case start: {caseType}");
-                        await EventSystem.Instance.Invoke<RobotInvokeArgs, ETTask>(caseType, new RobotInvokeArgs() { Content = content });
-                        RobotLog.Debug($"run case finish: {caseType}");
+                        await EventSystem.Instance.Invoke<RobotInvokeArgs, ETTask>(caseType, new RobotInvokeArgs() { Content = content});
+                        RobotLog.Debug($"---------run case finish: {caseType}");
                     }
                     catch (Exception e)
                     {
                         RobotLog.Debug($"run case error: {caseType}\n{e}");
+                        break;
                     }
-                    break;
                 }
-                case "RunAll":
-                {
-                    FieldInfo[] fieldInfos = typeof (RobotCaseType).GetFields();
-                    foreach (FieldInfo fieldInfo in fieldInfos)
-                    {
-                        int caseType = (int)fieldInfo.GetValue(null);
-                        if (caseType > RobotCaseType.MaxCaseType)
-                        {
-                            RobotLog.Debug($"case > {RobotCaseType.MaxCaseType}: {caseType}");
-                            break;
-                        }
-                        try
-                        {
-                            RobotLog.Debug($"run case start: {caseType}");
-                            await EventSystem.Instance.Invoke<RobotInvokeArgs, ETTask>(caseType, new RobotInvokeArgs() { Content = content});
-                            RobotLog.Debug($"---------run case finish: {caseType}");
-                        }
-                        catch (Exception e)
-                        {
-                            RobotLog.Debug($"run case error: {caseType}\n{e}");
-                            break;
-                        }
-                    }
-                    break;
-                }
+                break;
+            }
             }
             await ETTask.CompletedTask;
         }
