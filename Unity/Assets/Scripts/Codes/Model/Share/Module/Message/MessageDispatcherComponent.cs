88123adffdcef6ee7b56582098571af0be63bca7
deepwateroooMe <deepwaterooome@gmail.com>
2023-06-14 13:55:05 -0700

    LOVE MY DEAR COUSIN. WAITING FOR MARRIAGE...MUST MARRY HIM AS SOON AS POSSIBLE~~

--- Unity/Assets/Scripts/Codes/Model/Share/Module/Message/MessageDispatcherComponent.cs
+++ Unity/Assets/Scripts/Codes/Model/Share/Module/Message/MessageDispatcherComponent.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 namespace ET {
-
     // 总管：对每个场景，消息分发器
     // 这个类，可以简单地理解为：先前的各种服，现在的各种服务端场景，它们所拥有的消息处理器实例的封装。那么默认，每种场景，只有一个消息处理器实体类
     public class MessageDispatcherInfo { 
@@ -11,7 +10,6 @@ namespace ET {
             this.IMHandler = imHandler;
         }
     }
-    
     // 消息分发组件
     [ComponentOf(typeof(Scene))]
     public class MessageDispatcherComponent: Entity, IAwake, IDestroy, ILoad {
@@ -22,3 +20,6 @@ namespace ET {
         public readonly Dictionary<ushort, List<MessageDispatcherInfo>> Handlers = new(); // 总管的字典
     }
 }
+
+
+
--- Unity/Assets/Scripts/ThirdParty/ETTask/ETTask.cs
+++ Unity/Assets/Scripts/ThirdParty/ETTask/ETTask.cs
@@ -12,7 +12,7 @@ namespace ET {
                 return new ETTaskCompleted();
             }
         }
-        private static readonly Queue<ETTask> queue = new Queue<ETTask>();
+        private static readonly Queue<ETTask> queue = new Queue<ETTask>(); // 异步任务类：带一个异步任务的对象池队列
         // 请不要随便使用ETTask的对象池，除非你完全搞懂了ETTask!!!
         // 假如开启了池,await之后不能再操作ETTask，否则可能操作到再次从池中分配出来的ETTask，产生灾难性的后果
         // SetResult的时候请现将tcs置空，避免多次对同一个ETTask SetResult
