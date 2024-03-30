56cc2f03a5a7a53ba6959168a9e8975662ad5463
deepwateroooMe <deepwaterooome@gmail.com>
2024-03-30 10:27:23 -0700

    LOVE MY DEAR COUSIN. WAITING FOR MARRIAGE...MUST MARRY HIM AS SOON AS POSSIBLE~~

--- Unity/Assets/Scripts/Codes/Model/Share/Entry.cs
+++ Unity/Assets/Scripts/Codes/Model/Share/Entry.cs
@@ -1,43 +1,25 @@
-﻿namespace ET
-{
-    namespace EventType
-    {
-        public struct EntryEvent1
-        {
+﻿namespace ET {
+    namespace EventType {
+        public struct EntryEvent1 {
         }   
-        
-        public struct EntryEvent2
-        {
+        public struct EntryEvent2 {
         } 
-        
-        public struct EntryEvent3
-        {
+        public struct EntryEvent3 {
         } 
     }
-    
-    public static class Entry
-    {
-        public static void Init()
-        {
-            
+    public static class Entry {
+        public static void Init() {
         }
-        
-        public static void Start()
-        {
+        public static void Start() {
             StartAsync().Coroutine();
         }
-        
-        private static async ETTask StartAsync()
-        {
+        private static async ETTask StartAsync() {
             WinPeriod.Init();
-            
             MongoHelper.Init();
             ProtobufHelper.Init();
-            
-            Game.AddSingleton<NetServices>();
+            Game.AddSingleton<NetServices>(); // Mode 程序域里：服务端的【网络模块】。去看Game
             Game.AddSingleton<Root>();
             await Game.AddSingleton<ConfigComponent>().LoadAsync();
-
             await EventSystem.Instance.PublishAsync(Root.Instance.Scene, new EventType.EntryEvent1());
             await EventSystem.Instance.PublishAsync(Root.Instance.Scene, new EventType.EntryEvent2());
             await EventSystem.Instance.PublishAsync(Root.Instance.Scene, new EventType.EntryEvent3());
